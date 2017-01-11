72deb4bb3f93cf39867ab6c52e966610af43e6e6
Mohamad Raziff Azhar <raziff@Mohamads-MacBook-Pro.local>
2017-01-11 14:37:08 +0800

    added isBothEnabled

--- www/gps_check.js
+++ www/gps_check.js
@@ -7,6 +7,14 @@ module.exports = {
     	console.log('run isNetworkEnabled')
         cordova.exec(successCallback, errorCallback, "GPSCheck", "isNetworkEnabled", []);
     },
+    isBothEnabled: function (successCallback, errorCallback) {
+    	console.log('run isNetworkEnabled')
+        cordova.exec(function(networkRes){
+        	cordova.exec(function(gpsresult){
+        		successCallback([gpsresult,networkRes]);
+        	}, errorCallback, "GPSCheck", "isGPSEnabled", []);
+        }, errorCallback, "GPSCheck", "isNetworkEnabled", []);
+    },
     getLocation: function (successCallback, errorCallback) {
     	console.log('run getLocation')
         cordova.exec(successCallback, errorCallback, "GPSCheck", "getLocation", []);
