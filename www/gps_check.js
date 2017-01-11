d8e56a75f5e1026a1f3f744c57234364f51143f7
Mohamad Raziff Azhar <raziff@Mohamads-MacBook-Pro.local>
2017-01-11 12:12:38 +0800

    added js to pluginxml

--- www/gps_check.js
+++ www/gps_check.js
@@ -1,11 +1,15 @@
 module.exports = {
     isGPSEnabled: function (successCallback, errorCallback) {
+    	console.log('run isGPSEnabled')
         cordova.exec(successCallback, errorCallback, "GPSCheck", "isGPSEnabled", []);
     },
     isNetworkEnabled: function (successCallback, errorCallback) {
+    	console.log('run isNetworkEnabled')
         cordova.exec(successCallback, errorCallback, "GPSCheck", "isNetworkEnabled", []);
     },
     getLocation: function (successCallback, errorCallback) {
+    	console.log('run getLocation')
         cordova.exec(successCallback, errorCallback, "GPSCheck", "getLocation", []);
     }
+
 };
